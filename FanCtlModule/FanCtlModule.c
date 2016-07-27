a5e250788bb7d62f8108222127eb1d6eb5bd62ed
Roberto Guerrero <roberto.guerrero@gmail.com>
2016-07-27 14:14:14 -0500

    Agregue comentarios y cambie el delay de 1 seg por un schedule_timeout de 500 useg en el define MSEG 	modified:   FanCtlModule.c

--- FanCtlModule/FanCtlModule.c
+++ FanCtlModule/FanCtlModule.c
@@ -7,42 +7,48 @@
 #include <linux/thermal.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
-#include <linux/delay.h>
 
+#define MSECS 500 //Numero de milisegundos para dormir el hilo del kernel 
 
+//Estructura de kset
 static struct kset *fc_kset;
+//Estructura de tarea
 struct task_struct *task;
-int result = 0;
-int data;
+//Estructura de kobject
+static struct kobject *sensor_kobj;
+//Archivo dentro del kobject
+static unsigned long temp = 0;
+//Estructura de thermal_zone
+static struct thermal_zone_device *tz;
+//variable temporal para almacenar temperatura
 unsigned long temp_get;
 
-static unsigned long temp = 0;
-
-
-
+int result = 0;
+int data;
+//Funcion para leer el archivo
 static ssize_t temp_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
    return sprintf(buf, "%lu\n", temp);
 }
+//Funcion para escribir en el archivo
 static ssize_t temp_store(struct kobject *kobj, struct kobj_attribute *attr,
                                    const char *buf, size_t count){
    sscanf(buf, "%luu", &temp);
    return count;
 }
-
+//Atributos del archivo
 static struct kobj_attribute count_attr = __ATTR(temp, 0666, temp_show, temp_store);
-
+//Arreglo de atributos
 static struct attribute *fc_attrs[] = {
       &count_attr.attr,                 
       NULL,
 };
-
+//Estructura con el grupo de atributos
 static struct attribute_group attr_group = {
       .attrs = fc_attrs,                ///< The attributes array defined just above
 };
 
-static struct kobject *sensor_kobj;
-static struct thermal_zone_device *tz;
 
+//Funcion del hilo del kernel
 int thread_function(void *data){
 		int var=1;
 		while(!kthread_should_stop()){
@@ -50,15 +56,16 @@ int thread_function(void *data){
 		if(result){
 			printk(KERN_INFO "error al leer temp\n");
 		}
-		//printk (KERN_INFO "temp_get:%lu\n",temp_get);
+		printk (KERN_INFO "temp_get:%lu\n",temp_get);
 		temp=temp_get;
-		mdelay(1000);
-		schedule();
+		//mdelay(1000);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule_timeout(MSECS);
 	}
 	return var;
 }
 
-// Inicio del modulo
+//Inicio del modulo
 int fan_init(void)
 {
 	
@@ -66,27 +73,28 @@ int fan_init(void)
 	
 	printk(KERN_INFO "FanCtlModule: Se cargo el modulo\n");
 
-	// Creando kset para FanController en /sys/fan_controller
+	//Creando kset para FanController en /sys/fan_controller
        	fc_kset = kset_create_and_add("fan_controller", NULL, NULL);
 	if (!fc_kset)
 		return -ENOMEM;
-
-	sensor_kobj = kobject_create_and_add("sensor", &fc_kset->kobj); // kernel_kobj points to /sys/kernel
+	//Creando kobject dentro de kset
+	sensor_kobj = kobject_create_and_add("sensor", &fc_kset->kobj);
 	if(!sensor_kobj){
-		printk(KERN_ALERT "EBB Button: failed to create kobject mapping\n");
+		printk(KERN_ALERT "FanCtlModule: failed to create kobject mapping\n");
 	return -ENOMEM;
 	}
-        
+    //Crendo archivo dentro de kobject
 	result = sysfs_create_group(sensor_kobj, &attr_group);
 	if(result) {
 		printk(KERN_ALERT "FanCtl: failed to create sysfs group\n");
 		kobject_put(sensor_kobj);// clean up -- remove the kobject sysfs entry
 	return result;
    	}
-	tz=thermal_zone_get_zone_by_name ("imx_thermal_zone");//obtiene el thermal_zone_device con nombre "imx_thermal_zone" que genera el 
+	//Funcion para obtener estructura de thermal_zone buscando de la lista de zonas por el nombre de "imx_thermal_zone"
+	tz=thermal_zone_get_zone_by_name ("imx_thermal_zone");
 	printk(KERN_INFO "type: %s\n",tz->type);
 	printk(KERN_INFO "trips: %d\n",tz->trips);
-
+	//Crea y agrega el hilo de kernel a la lista de procesos
 	task=kthread_run(&thread_function,&data,"FanCtlModule");
 
 	return 0;
@@ -95,14 +103,16 @@ int fan_init(void)
 // Salida del modulo
 void fan_cleanup(void)
 {
-  // Destruyendo kset
-  kobject_put(sensor_kobj);
-  kset_unregister(fc_kset);
-  printk(KERN_INFO "FanCtlModule: Se libero el modulo\n");
-  kthread_stop(task);
+  	//Destruye kobject decrementando el contador de instancias
+	kobject_put(sensor_kobj);
+	//Destruyendo kset
+	kset_unregister(fc_kset);
+	//Detiene el hilo del kernel
+	kthread_stop(task);
+	printk(KERN_INFO "FanCtlModule: Se libero el modulo\n");
 }
 
-// Especificando las funciones de inicio y fin del modulo
+//Especificando las funciones de inicio y fin del modulo
 module_init(fan_init);
 module_exit(fan_cleanup);
 MODULE_LICENSE("GPL v2");
